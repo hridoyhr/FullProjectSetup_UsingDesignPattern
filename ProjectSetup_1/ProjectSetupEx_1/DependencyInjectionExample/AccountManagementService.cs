b69a3be391a0f18dae01b1f9528afd6abde1bc4b
hridoyhr <hridoyswe205@gmail.com>
2021-08-18 00:00:04 +0600

    Unit of work and repository pattern

---
+++ ProjectSetup_1/ProjectSetupEx_1/DependencyInjectionExample/AccountManagementService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionExample
+{
+    public class AccountManagementService
+    {
+        public void CreateAccount(string email, string password)
+        {
+            if(!string.IsNullOrEmpty(email)  && !string.IsNullOrEmpty(password))
+            {
+                try
+                {
+                    SaveUserInDatabase(email, password);
+                }
+                catch
+                {
+                    SendEamilToUser(email);
+                }
+            }
+        }
+
+        private void SaveUserInDatabase(string email, string password)
+        {
+            //code to add user in database
+        }
+
+        private void SendEamilToUser(string email)
+        {
+            //send welcome email to user
+        }
+    }
+}
