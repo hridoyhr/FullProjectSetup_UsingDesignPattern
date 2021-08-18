f3f3468481cc1cb83fb690eff0878e87a44143f3
hridoyhr <hridoyswe205@gmail.com>
2021-08-18 17:35:27 +0600

    unit of work and repository pattern

---
+++ AttendanceSystem/AttendanceDemo.Training/UnitOfWorks/TrainingUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using AttendanceDemo.Data;
+using AttendanceDemo.Training.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceDemo.Training.UnitOfWorks
+{
+    public class TrainingUnitOfWork<T> : UnitOfWork where T : DbContext
+    {
+        public IRepository<Student> Students { get; private set; }
+
+        public IRepository<Attendance> Attendances { get; private set; }
+
+        public TrainingUnitOfWork(T context) : base(context)
+        {
+        }
+    }
+}
